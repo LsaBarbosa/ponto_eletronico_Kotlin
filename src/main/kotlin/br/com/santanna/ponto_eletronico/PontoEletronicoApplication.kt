d546a5eefc1ac69454936970fb9c1c40060ffc81
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-06-17 17:57:29 -0300

    Start Project

---
+++ src/main/kotlin/br/com/santanna/ponto_eletronico/PontoEletronicoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.santanna.ponto_eletronico
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PontoEletronicoApplication
+
+fun main(args: Array<String>) {
+    runApplication<PontoEletronicoApplication>(*args)
+}
