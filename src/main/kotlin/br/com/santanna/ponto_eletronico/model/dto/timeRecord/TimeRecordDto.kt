29d19c8af6ec83e6df7557e5b46ccef62a31b157
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-06-21 10:30:00 -0300

    removendo dto

--- src/main/kotlin/br/com/santanna/ponto_eletronico/model/dto/timeRecord/TimeRecordDto.kt
+++
@@ -1,8 +0,0 @@
-package br.com.santanna.ponto_eletronico.model.dto.timeRecord
-
-data class TimeRecordDto(
-    val id: Long?,
-    val startWorkTime: String?,
-    val endWorkTime: String?,
-    val workTime: String?
-)
