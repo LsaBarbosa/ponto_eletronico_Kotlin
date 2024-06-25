054efe82945c06ff642803a5e6fcad29b0a451a2
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-06-24 22:57:20 -0300

    tramaeno null

--- src/main/kotlin/br/com/santanna/ponto_eletronico/model/dto/company/CompanyDTO.kt
+++ src/main/kotlin/br/com/santanna/ponto_eletronico/model/dto/company/CompanyDTO.kt
@@ -6,5 +6,5 @@ data class CompanyDTO(
     var id: Long? = null,
     var nameCompany:String? = null,
     var companyCNPJ: String? = null,
-    val employees: List<SimpleEmployeeDto> = ArrayList(),
+    val employees: List<SimpleEmployeeDto>? = ArrayList(),
 )
