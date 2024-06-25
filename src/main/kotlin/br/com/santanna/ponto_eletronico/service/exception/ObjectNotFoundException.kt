db178a5af43f5a23fa19ffe8fe5ca88f85c5bbbe
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-06-24 22:58:57 -0300

    construtor

--- src/main/kotlin/br/com/santanna/ponto_eletronico/service/exception/ObjectNotFoundException.kt
+++ src/main/kotlin/br/com/santanna/ponto_eletronico/service/exception/ObjectNotFoundException.kt
@@ -1,5 +1,4 @@
 package br.com.santanna.ponto_eletronico.service.exception
 
-class ObjectNotFoundException:RuntimeException {
-    constructor(message: String?): super(message)
+class ObjectNotFoundException(message: String?) : RuntimeException(message) {
 }
