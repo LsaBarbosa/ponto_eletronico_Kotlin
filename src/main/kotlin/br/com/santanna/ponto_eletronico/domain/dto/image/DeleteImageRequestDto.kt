ea26728b5ec221a66a8eaa995404b071b7fe2faf
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-07-14 10:00:48 -0300

    refector img remove manager password

--- src/main/kotlin/br/com/santanna/ponto_eletronico/domain/dto/image/DeleteImageRequestDto.kt
+++ src/main/kotlin/br/com/santanna/ponto_eletronico/domain/dto/image/DeleteImageRequestDto.kt
@@ -3,6 +3,5 @@ package br.com.santanna.ponto_eletronico.domain.dto.image
 data class DeleteImageRequestDto(
     val imageId: Long,
     val employeeCpfTarget: String,
-    val employeeManagerCpf: String,
     val passwords: String
 )
