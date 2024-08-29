96f64a1c353952a0c8763ac0cdfdb4476b01fe34
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-08-29 17:37:26 -0300

    url cross Qa delete user

--- src/main/kotlin/br/com/santanna/ponto_eletronico/infrastructure/security/WebSecurityConfig.kt
+++ src/main/kotlin/br/com/santanna/ponto_eletronico/infrastructure/security/WebSecurityConfig.kt
@@ -73,7 +73,7 @@ class WebSecurityConfig (
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
         config.allowCredentials = true
-        config.allowedOrigins = listOf("https://kronos-pearl.vercel.app/") // Substitua pela URL do seu front-end
+        config.allowedOrigins = listOf("https://kronos-cw4x.onrender.com") // Substitua pela URL do seu front-end
         config.allowedHeaders = listOf("*")
         config.allowedMethods = listOf("*")
         source.registerCorsConfiguration("/**", config)
