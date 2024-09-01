0902221a5ba57d7d8172040b4710e0008d30c24f
LsaBarbosa <l.sant.anna.b@gmail.com>
2024-09-01 11:43:21 -0300

    cors url em variavel de ambiente

--- src/main/kotlin/br/com/santanna/ponto_eletronico/infrastructure/security/WebSecurityConfig.kt
+++ src/main/kotlin/br/com/santanna/ponto_eletronico/infrastructure/security/WebSecurityConfig.kt
@@ -2,6 +2,7 @@ package br.com.santanna.ponto_eletronico.infrastructure.security
 
 import br.com.santanna.ponto_eletronico.infrastructure.security.jwt.JwtAuthenticationEntryPoint
 import br.com.santanna.ponto_eletronico.infrastructure.security.jwt.JwtRequestFilter
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
@@ -21,8 +22,9 @@ import org.springframework.web.filter.CorsFilter
 @EnableWebSecurity
 class WebSecurityConfig (
     private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
-
-    private val jwtRequestFilter: JwtRequestFilter
+    private val jwtRequestFilter: JwtRequestFilter,
+    @Value("\${CORS.URL}")
+    private val corsURL: String
 ) {
 
     @Bean
@@ -73,7 +75,7 @@ class WebSecurityConfig (
         val source = UrlBasedCorsConfigurationSource()
         val config = CorsConfiguration()
         config.allowCredentials = true
-        config.allowedOrigins = listOf("https://kronos-cw4x.onrender.com") // Substitua pela URL do seu front-end
+        config.allowedOrigins = listOf(corsURL) // Substitua pela URL do seu front-end
         config.allowedHeaders = listOf("*")
         config.allowedMethods = listOf("*")
         source.registerCorsConfiguration("/**", config)
