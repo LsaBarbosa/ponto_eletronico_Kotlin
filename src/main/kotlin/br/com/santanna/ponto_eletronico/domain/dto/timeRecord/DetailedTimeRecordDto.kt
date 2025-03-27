b5da0fc99590ad5acc2d1d39f7351505abc17643
LsaBarbosa <l.sant.anna.b@gmail.com>
2025-03-27 18:19:34 -0300

    feature marcador de edição de registro

--- src/main/kotlin/br/com/santanna/ponto_eletronico/domain/dto/timeRecord/DetailedTimeRecordDto.kt
+++ src/main/kotlin/br/com/santanna/ponto_eletronico/domain/dto/timeRecord/DetailedTimeRecordDto.kt
@@ -7,4 +7,5 @@ data class DetailedTimeRecordDto(
     val startWorkDate: String? = null,
     val endWorkDate: String? = null,
     val timeWorked: String? = null,
+    val edited: Boolean = false
 )
